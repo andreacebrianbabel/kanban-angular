d3e4a84b6dbbf7c24771eeb17c217c4f74e61efc
andreacebrianbabel <andrea.cebrian@babelgroup.com>
2023-02-27 17:11:15 +0100

    kanban angular

--- kanban-angular/src/app/app-routing.module.ts
+++ kanban-angular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { KanbanComponent } from './kanban-dashboard/kanban/kanban.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: KanbanComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- kanban-angular/src/app/app.module.ts
+++ kanban-angular/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { KanbanDashboardModule } from './kanban-dashboard/kanban-dashboard.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KanbanDashboardModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
